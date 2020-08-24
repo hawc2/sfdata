f620dce3ea7fe374b1575c1eba0cf1dfb4270415
hawc2 <alwermercolan@gmail.com>
2020-08-23 21:27:33 -0400

    building out flask app

--- sfdata.py
+++ sfdata.py
@@ -1,6 +1,22 @@
+from datetime import datetime
+
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def welcome():
     return 'Welcome to the SF Nexus!'
+
+@app.route('/date')
+def date():
+    return "This page was served at " + str(datetime.now())
+
+counter = 0
+
+@app.route("/count_views")
+def count_demo():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"
